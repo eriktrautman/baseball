f986ef20eb53a7b74d021f1c2dc46f7603bc38ad
Erik Trautman and Niranjan Ramadas <erik+niranjan@appacademy.io>
2013-01-29 09:13:18 -0800

    initial commit

---
+++ baseball/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baseball::Application.config.secret_token = 'bec2cf4e85c5575a959b55c593d1e52b94642169df89e34ff12ef21edbd1e50f932e412d351aedc36e410706de6d6b9949793c59b17c8c37b3d36c2a5af2c4ef'
